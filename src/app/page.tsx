1cb0c85afe256273f91ed1088dbec0de8e0e253f
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-10 10:58:57 +1300

    using material ui for page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,24 @@ import SearchBar from "@/components/SearchBarComponent";
 import FilterOptions from "@/components/FilterComponent";
 import Results from "@/components/ResultsComponent";
 
+import {Box} from "@mui/material"
+
 export default function Home() {
     return (
-        <div>
-            <div className={"title-div"}>
+        <Box p = {3}>
+            <Box className={"title-div"}>
                 <h1 className={"title"}>Object Library</h1>
-            </div>
+            </Box>
 
-            <div className={"content-div"}>
+            <Box className={"content-div"}>
 
-                <div className={"search-div"}>
+                <Box className={"search-div"}>
                     <SearchBar/>
                     <FilterOptions/>
-                </div>
+                </Box>
 
                 <Results/>
-            </div>
-        </div>
+            </Box>
+        </Box>
     );
 }
