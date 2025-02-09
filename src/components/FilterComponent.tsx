34a13eedbe1fd172ef90368b8300a6a24f59da25
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-10 10:57:23 +1300

    adding more to the filter component

--- src/components/FilterComponent.tsx
+++ src/components/FilterComponent.tsx
@@ -1,30 +1,60 @@
-"use client"
+"use client";
 
+import api from "../api";
 import {SubmitHandler, useForm} from "react-hook-form";
+import {TextField, Button, Box} from "@mui/material";
 
-interface FormValues {
+
+class FormValues {
     material: string;
     dimensions: {
         length: number;
         width: number;
         height: number;
     };
+
+    constructor(material: string, dimensions: { length: number; width: number; height: number }) {
+        this.material = material;
+        this.dimensions = dimensions;
+    }
+
+    static toFlatObject(formValues: FormValues) {
+        return {
+            material: formValues.material,
+            "dimensions.height": formValues.dimensions.height,
+            "dimensions.width": formValues.dimensions.width,
+            "dimensions.length": formValues.dimensions.length
+        };
+    }
 }
+
+function getFilterResults(formValues: FormValues) {
+    const apiResponse = api.get("/object/filter", {
+        params: FormValues.toFlatObject(formValues)
+    });
+
+    console.log(apiResponse);
+}
+
 
 export default function FilterOptions() {
     const {register, handleSubmit} = useForm<FormValues>();
 
-    const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => console.log(data);
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log("Filtering with data: ", data);
+        getFilterResults(data);
+    };  
 
     return (
-        <div className="filters-div">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("material")} type="string" placeholder="Material"/>
-                <input {...register("dimensions.height")} type="number" placeholder="Height"/>
-                <input {...register("dimensions.width")} type="number" placeholder="Width"/>
-                <input {...register("dimensions.length")} type="number" placeholder="Length"/>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)}
+             sx={{display: "flex", flexDirection: "column", gap: 2, maxWidth: 300}}>
+            <TextField {...register("material")} label="Material" variant="outlined"/>
+
+            <TextField {...register("dimensions.height")} label="Height" type="number" variant="outlined"/>
+            <TextField {...register("dimensions.width")} label="Width" type="number" variant="outlined"/>
+            <TextField {...register("dimensions.length")} label="Length" type="number" variant="outlined"/>
+
+            <Button type="submit" variant="contained">Submit</Button>
+        </Box>
     );
 }
