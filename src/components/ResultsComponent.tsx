ed387d4a639d5b5767f759f2a56874ca9addc1e3
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-10 10:58:33 +1300

    getting something going with the results component

--- src/components/ResultsComponent.tsx
+++ src/components/ResultsComponent.tsx
@@ -1,9 +1,36 @@
-"use client"
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
-export default function Results() {
-    return (
-        <div className="results-div">
-            <h1>Results</h1>
-        </div>
-    );
+const columns = [
+  { id: 'id', label: 'ID' },
+  { id: 'name', label: 'Name' },
+];
+
+const rows = [
+    { id: 3, name: 'Item 3' },
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+];
+
+export default function MyTable() {
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell key={column.id}>{column.label}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.id}</TableCell>
+              <TableCell>{row.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
