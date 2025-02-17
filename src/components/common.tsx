ed568510b1e8bf8a0eb5b9cff1a892fcead5c940
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-18 11:59:50 +1300

    adding a general download cell for files in a table

--- src/components/common.tsx
+++ src/components/common.tsx
@@ -3,6 +3,9 @@
 import React, {useEffect, useState} from 'react';
 import api from "@/api";
 import Image from "next/image";
+import {saveAs} from "file-saver";
+import {Button, TableCell} from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
 
 export function ObjectImage({object_id, width, height}: { object_id: string, width: number, height: number }) {
     const [imageUrl, setImageUrl] = useState<string | null>(null);
@@ -10,7 +13,7 @@ export function ObjectImage({object_id, width, height}: { object_id: string, wid
     const apiURL = `/object/${object_id}/photo`;
 
     useEffect(() => {
-        api.get(apiURL, { responseType: "blob" })
+        api.get(apiURL, {responseType: "blob"})
             .then((response) => {
                 setImageUrl(URL.createObjectURL(response.data));
             })
@@ -21,5 +24,33 @@ export function ObjectImage({object_id, width, height}: { object_id: string, wid
         return <span>Loading...</span>;
     }
 
-    return <Image src={imageUrl} alt="Object" width={width} height={height} />;
+    return <Image src={imageUrl} alt="Object" width={width} height={height}/>;
+}
+
+export function DownloadTableCell({object_id, handleDownload, label}: {
+    object_id: string,
+    handleDownload: any,
+    label: string
+}) {
+
+    return <TableCell>
+        <Button variant="contained" size="small" startIcon={<DownloadIcon/>}
+                onClick={() => handleDownload(object_id)}>{label}
+        </Button>
+    </TableCell>
+}
+
+export function IfcDownloadTableCell({object_id}: { object_id: string }) {
+
+    async function handleDownload(object_id: string) {
+        try {
+            const apiResponse = await api.get(`/object/${object_id}`, {params: {format: "ifc"}})
+            const blob = new Blob([apiResponse.data])
+            saveAs(blob, `${object_id}.ifc`)
+        } catch (e) {
+            console.error("Failed to download file: ", e)
+        }
+    }
+
+    return <DownloadTableCell object_id={object_id} handleDownload={handleDownload} label={"IFC"}/>
 }
