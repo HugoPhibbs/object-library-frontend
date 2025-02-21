d868c974ee9bb0161301ad6693f71b639b4d2c8b
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-21 15:39:22 +1300

    adding a mui theme to the app

---
+++ src/app/theme-registry.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ReactNode } from "react";
+import {green} from "@mui/material/colors";
+
+const theme = createTheme({
+    palette: {
+        success: {
+            main: green[500],
+        },
+    },
+});
+
+export default function ThemeRegistry({ children }: { children: ReactNode }) {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
+}
