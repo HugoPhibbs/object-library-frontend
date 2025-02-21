f55d9e92b7689897d7314e2f12c128d0495f39ab
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-21 15:40:25 +1300

    adding a sort dropdown

--- src/components/search-filter/ResultsComponent.tsx
+++ src/components/search-filter/ResultsComponent.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {
-    Box,
-    Link as MuiLink,
+    Box, FormControl, InputLabel,
+    Link as MuiLink, MenuItem, Paper, Select,
     Table,
     TableBody,
     TableCell,
@@ -14,6 +14,7 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import Link from "next/link";
 import {IfcDownloadTableCell, ObjectImage} from "@/components/common";
 import {FilteredLibraryObject, LibraryObject} from "@/utils";
+import _, {orderBy} from "lodash";
 
 // Define columns
 
@@ -25,7 +26,7 @@ type ColumnData = {
 
 const columns: ColumnData[] = [
     {id: 'id', label: 'ID', canSort: false},
-    {id: 'name', label: 'Name', canSort: true},
+    {id: 'name', label: 'Name', canSort: false},
     // {id: "ifc_type", label: "IFC Type", canSort: false},
     {id: "material", label: "Material", canSort: false},
     // {id: "load_bearing", label: "Load Bearing", canSort: true},
@@ -36,16 +37,11 @@ const columns: ColumnData[] = [
 
 function Row({object}: { object: LibraryObject }) {
     return (
-        <TableRow>
+        <TableRow hover>
             <TableCell><ViewObjectLink object_id={object.id}/></TableCell>
             <TableCell>{object.name}</TableCell>
-            {/*<TableCell>{row.ifc_type}</TableCell>*/}
             <TableCell>{object.material}</TableCell>
-            {/*<TableCell>{booleanToYesNo(row.load_bearing)}</TableCell>*/}
-            {/*<TableCell>{row.form_factor}</TableCell>*/}
-            <TableCell>
-                <ObjectImage object_id={object.id} width={200} height={200}/>
-            </TableCell>
+            <TableCell><ObjectImage object_id={object.id} width={200} height={200}/></TableCell>
             <IfcDownloadTableCell object_id={object.id}/>
         </TableRow>
     );
@@ -79,41 +75,72 @@ function SortableTableHeadCell({column, orderBy, direction, onClick}: {
     );
 }
 
-export default function Results({data}: {data: FilteredLibraryObject[]}) {
-    const [sortedData, setsortedData] = useState<FilteredLibraryObject[]>([]);
-    const [orderBy, setOrderBy] = useState("id");
-    const [order, setOrder] = useState<"asc" | "desc">("asc");
+function SortByDropdown({orderBy, setOrderBy}: {
+    orderBy: string,
+    setOrderBy: (value: string) => void
+}) {
 
-    useEffect(() => {
-        setsortedData(data.toSorted((object_a: FilteredLibraryObject, object_b: FilteredLibraryObject) => {
-                return order === "asc" ? object_a.score - object_b.score : object_b.score - object_a.score;
-            })
-        );
-    }, [data, order]);
+    const dropdownChange = (e: any) => {
+        setOrderBy(e.target.value)
+    }
+
+    const dropdownOptions = [
+        {value: "score-desc", label: "Relevancy (desc)"},
+        {value: "score-asc", label: "Relevancy (asc)"},
+        {value: "name-desc", label: "Name (desc)"},
+        {value: "name-asc", label: "Name (asc)"},
+        {value: "material-desc", label: "Material (desc)"},
+        {value: "material-asc", label: "Material (asc)"},
+    ]
 
     return (
-        <TableContainer>
-            <Table className={"data-table"}>
-                <TableHead>
-                    <TableRow>
-                        {columns.map((column) => (
-                            // TODO, find a way to disable sorting, i.e. toggle asc, desc, none
-                            <SortableTableHeadCell key={column.id} column={column} orderBy={orderBy}
-                                                   direction={order}
-                                                   onClick={() => {
-                                                       setOrderBy(column.id);
-                                                       setOrder(order === "asc" ? "desc" : "asc");
-                                                   }}/>
+        <FormControl id="sort-by-dropdown-control">
+            <InputLabel>Sort By</InputLabel>
+
+            <Select value={orderBy} onChange={dropdownChange} label={"Sort By"}>
+                {dropdownOptions.map((option, index) => (
+                    <MenuItem key={index} value={option.value}>{option.label}</MenuItem>))}
+            </Select>
+        </FormControl>
+    );
+}
+
+export default function Results({data}: { data: FilteredLibraryObject[] }) {
+    const [sortedData, setSortedData] = useState<FilteredLibraryObject[]>([]);
+    const [orderBy, setOrderBy] = useState("score-desc");
+
+    useEffect(() => {
+        const [attributeToSort, sortOrder] = orderBy.split("-") as [string, "asc" | "desc"];
+
+        const path = attributeToSort == "score" ? "score" : "object." + attributeToSort;
+
+        setSortedData(_.orderBy(data, [path], [sortOrder]));
+    }, [data, orderBy]);
+
+    return (
+        <Box id="results-box">
+            <Box>
+                <SortByDropdown orderBy={orderBy} setOrderBy={setOrderBy}/>
+            </Box>
+            <TableContainer>
+
+                <Table className={"data-table"} stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column, index) => (
+                                <TableCell key={index} className={"table-column-header-cell"}>
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {sortedData.map((object: FilteredLibraryObject) => (
+                            <Row key={object.object.id} object={object.object}/>
                         ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {sortedData.map((object: FilteredLibraryObject) => (
-                        <Row key={object.object.id} object={object.object}/>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
     )
-        ;
 }
