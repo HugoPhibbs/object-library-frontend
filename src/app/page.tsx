c334ce024578ff3cf010f1cbdf216a279990896d
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-21 15:39:49 +1300

    optimizing imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 // import Image from "next/image";
 import SearchObjects from "@/components/search-filter/SearchAndResultsComponent";
 
-import {Box, Typography} from "@mui/material"
-import TopNavBar from "@/components/TopNavBar";
-
 export default function Home() {
     return (
         <>
