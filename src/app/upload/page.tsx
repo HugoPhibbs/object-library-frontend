2b092ff74651c920a5a717cffd2a16bb52d620d3
Hugo Phibbs <hugophibbs1@gmail.com>
2025-02-21 15:39:41 +1300

    upgrading the upload page

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
-import {Box, Button, Typography} from "@mui/material";
+import {Box, Button, FormControlLabel, TextField, Typography} from "@mui/material";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import TopNavBar from "@/components/TopNavBar";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import {styled} from '@mui/material/styles';
 import {useState} from "react";
 import api from "@/api";
-import {FileOpen} from "@mui/icons-material";
+import {CheckCircle, FileOpen} from "@mui/icons-material";
+import {green} from "@mui/material/colors";
+import {throwWithStaticGenerationBailoutErrorWithDynamicError} from "next/dist/server/request/utils";
+
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -20,7 +25,7 @@ const VisuallyHiddenInput = styled('input')({
     width: 1,
 });
 
-function PickFileButton({onChange}: { onChange?: (event: any) => void }) {
+function PickFileButton({onChange}: { onChange?: (event: any) => void}) {
     return (
         <Button
             id={"pick-file-button"}
@@ -41,7 +46,24 @@ function PickFileButton({onChange}: { onChange?: (event: any) => void }) {
     );
 }
 
-function UploadButton({onClick, disabled, fileName}: { onClick: () => void, disabled: boolean, fileName?: string }) {
+function FileUploadIcon({fileUploaded, fileUploadError}: { fileUploaded: boolean, fileUploadError: boolean }) {
+    if (fileUploaded) {
+        return <CheckCircleIcon/>
+    } else if (fileUploadError) {
+        return <HighlightOffIcon color={"error"}/>
+    } else {
+        return <CloudUploadIcon/>
+    }
+}
+
+function UploadButton({onClick, disabled, fileName, fileUploaded, fileUploadError, fileUpdatedOrCreated}: {
+    onClick: () => void,
+    disabled: boolean,
+    fileName?: string,
+    fileUploaded: boolean,
+    fileUploadError: boolean,
+    fileUpdatedOrCreated: boolean
+}) {
     return (
         <Button
             id={"upload-file-button"}
@@ -49,62 +71,110 @@ function UploadButton({onClick, disabled, fileName}: { onClick: () => void, disa
             role={undefined}
             variant="contained"
             tabIndex={-1}
-            startIcon={<CloudUploadIcon/>}
+            startIcon={<FileUploadIcon fileUploaded={fileUploaded} fileUploadError={fileUploadError}/>}
+            color={fileUploaded ? "success" : fileUploadError ? "error" : "primary"}
+            sx={(theme) => ({
+                color: "white",
+                border: `3px solid ${fileUploaded ? green[500] : fileUploadError ? "red" : null}`,
+                "&.Mui-disabled": {
+                    backgroundColor: fileUploaded ? theme.palette.success.main : "light-grey",
+                    color: fileUploaded ? "white" : "inherit",
+                }
+            })}
             onClick={onClick}
             disabled={disabled}
         >
-            Save
-            {fileName ? <StyledFileName fileName={fileName}/> : null}
+            {fileUploaded ? fileUpdatedOrCreated ? "Updated" : "Created" : fileUploadError ? "Error" : "Upload"}
+            {fileName ? (
+                <Typography component="span" id={"styled-file-name"}>
+                    {fileName}
+                </Typography>
+            ) : null}
         </Button>
     );
-}
-
-function StyledFileName({fileName}: { fileName: string }) {
-    return (
-        <Typography component="span" id={"styled-file-name"}>
-            {fileName}
-        </Typography>
-    )
 }
 
 export default function UploadPage() {
 
     const [file, setFile] = useState<File | null>(null);
     const [fileName, setFileName] = useState<string>("");
+    const [customID, setCustomID] = useState<string>("");
+    const [fileUploaded, setFileUploaded] = useState<boolean>(false);
+    const [fileUploadError, setFileUploadError] = useState<boolean>(false);
+    const [fileUpdatedOrCreated, setFileUpdatedOrCreated] = useState<boolean>(false);
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
             const newFile = event.target.files[0];
             setFile(newFile);
             setFileName(newFile.name);
+            setFileUploaded(false);
+            setFileUploadError(false);
+            setFileUpdatedOrCreated(false);
         }
     };
 
     const handleUpload = () => {
         if (file) {
             const formData = new FormData();
+
             formData.append("file", file);
+            if (customID) formData.append('customID', customID);
 
             api.post("/object", formData, {
                 headers: {
                     "Content-Type": "multipart/form-data"
                 }
+            }).then((response) => {
+                setFileUploaded(true);
+                setFileUploadError(false);
+
+                if (response.status === 201) setFileUpdatedOrCreated(false); else setFileUpdatedOrCreated(true);
+
+            }).catch((error) => {
+                console.error("Failed to upload file: ", error);
+                setFileUploadError(true);
+                setFileUploaded(false);
             });
         }
+    }
+
+    const customIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCustomID(event.target.value);
+        setFileUploaded(false);
+        setFileUploadError(false);
+        setFileUpdatedOrCreated(false);
     }
 
     return (
         <>
             <Box id={"main-upload-box"}>
-                <Box>
+                <Box id="upload-info-box">
                     <h2>
                         Upload an IFC file
                     </h2>
+
+                    <p>
+                        Upload an IFC file to add it to the object library. Optionally, you can provide a custom ID to
+                        associate with the file.
+
+                        If an object with the same ID already exists, it will be updated with the new file.
+                    </p>
                 </Box>
 
                 <Box id={"pick-upload-button-box"}>
                     <PickFileButton onChange={handleFileChange}/>
-                    <UploadButton onClick={handleUpload} disabled={!fileName} fileName={fileName}/>
+                    <TextField onChange={customIdChange} label={"Enter Custom ID"}></TextField>
+
+                    <Box>
+                        <UploadButton onClick={handleUpload}
+                                      disabled={!fileName || fileUploaded || fileUploadError}
+                                      fileName={fileName}
+                                      fileUploaded={fileUploaded}
+                                      fileUploadError={fileUploadError}
+                                      fileUpdatedOrCreated={fileUpdatedOrCreated}
+                        />
+                    </Box>
                 </Box>
 
                 {/*{file ? <p><StyledFileName fileName={fileName}/> added</p> : null}*/}
