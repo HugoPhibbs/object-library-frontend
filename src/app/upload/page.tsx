7e40d62c0ea2408deaa668456ff871459f83acf1
Hugo Phibbs <hugophibbs1@gmail.com>
2025-03-14 14:59:24 +1300

    adding support to add inspection records when uploading an object

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,16 +1,23 @@
 "use client"
 
-import {Box, Button, TextField, Typography} from "@mui/material";
+import {Box, Button, FormControl, IconButton, InputLabel, TextField, Typography} from "@mui/material";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import {styled} from '@mui/material/styles';
-import {useState} from "react";
+import {Component, ReactNode, useEffect, useState} from "react";
 import api from "@/api";
 import {green} from "@mui/material/colors";
 import {IfcIcon} from "@/components/common";
+import {AttachFile} from "@mui/icons-material";
+import {DatePicker, LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import {Dayjs} from "dayjs";
 
+// For uploading files
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
     clipPath: 'inset(50%)',
@@ -23,11 +30,16 @@ const VisuallyHiddenInput = styled('input')({
     width: 1,
 });
 
+type InspectionRecord = {
+    date: Dayjs | null,
+    file?: File
+}
+
 
 function PickFileButton({onChange, label, StartIcon, accept_types}: {
-    onChange?: (event: any) => void,
+    onChange: (event: any) => void,
     label: string,
-    StartIcon: React.ElementType,
+    StartIcon: ReactNode,
     accept_types: string[]
 }) {
     return (
@@ -37,7 +49,7 @@ function PickFileButton({onChange, label, StartIcon, accept_types}: {
             role={undefined}
             variant="contained"
             tabIndex={-1}
-            startIcon={<StartIcon/>}
+            startIcon={StartIcon}
         >
             {label}
             <VisuallyHiddenInput
@@ -98,50 +110,195 @@ function UploadButton({onClick, disabled, fileName, fileUploaded, fileUploadErro
     );
 }
 
+function UploadSingleInspectionRecord({record, dateOnChange, removeOnClick, fileOnChange}: {
+    record: { date: Dayjs | null; file?: File },
+    dateOnChange: (newValue: Dayjs | null) => void,
+    fileOnChange: (file: File) => void,
+    removeOnClick: () => void
+}) {
+    return (
+        <Box className="single-inspection-record-box">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                    views={["year", "month"]}
+                    label="Month & Year"
+                    value={record.date}
+                    onChange={dateOnChange}
+                    className="inspection-date-picker"
+                />
+            </LocalizationProvider>
+
+            <PickFileButton onChange={(e) => fileOnChange(e.target.files?.[0])} label="PDF" StartIcon={<AttachFile/>}
+                            accept_types={[".pdf"]}/>
+
+            <IconButton color="error" onClick={removeOnClick}>
+                <RemoveIcon/>
+            </IconButton>
+        </Box>
+    );
+}
+
+function UploadInspectionRecords({inspectionRecords, setInspectionRecords}: {
+    inspectionRecords: InspectionRecord[],
+    setInspectionRecords: any
+}) {
+
+    const handleAddRecord = () => {
+        setInspectionRecords([...inspectionRecords, {date: null, file: undefined}]);
+    }
+
+    const handleRemoveRecord = (index: number) => {
+        const newRecords = [...inspectionRecords];
+        newRecords.splice(index, 1);
+        setInspectionRecords(newRecords);
+    }
+
+    const handleDateChange = (index: number, date: Dayjs | null) => {
+        const newRecords = [...inspectionRecords];
+        newRecords[index].date = date;
+        setInspectionRecords(newRecords);
+    }
+
+    const handleFileChange = (index: number) => {
+        return (file: File) => {
+            const newRecords = [...inspectionRecords];
+            newRecords[index].file = file;
+            setInspectionRecords(newRecords)
+        }
+    }
+
+    return (
+        <Box id={"inspection-records-box"}>
+            <Typography variant={"h3"}>Inspection Records</Typography>
+
+            <Box>
+                {inspectionRecords.map((record, index) => (
+                    <UploadSingleInspectionRecord key={index} record={record}
+                                                  dateOnChange={(newValue) => handleDateChange(index, newValue)}
+                                                  removeOnClick={() => handleRemoveRecord(index)}
+                                                  fileOnChange={(file: File) => handleFileChange(index)(file)}
+                    />
+                ))
+                }
+            </Box>
+
+            <Button startIcon={<AddIcon/>} onClick={handleAddRecord}>Add</Button>
+        </Box>
+    )
+}
+
+
+function postIFCFile(ifcFile: File, customID: string, setFileUploaded: any, setFileUploadError: any, setFileUpdatedOrCreated: any, setCreatedObjectId: any) {
+    const formData = new FormData();
+
+    formData.append("file", ifcFile);
+    if (customID) formData.append('customID', customID);
+
+    api.post("/object", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    }).then((response) => {
+        setFileUploaded(true);
+        setFileUploadError(false);
+
+        const newObjectId = response.data.object_id
+        setCreatedObjectId(newObjectId);
+
+        if (response.status === 201) setFileUpdatedOrCreated(false); else setFileUpdatedOrCreated(true);
+
+    }).catch((error) => {
+        console.error("Failed to upload file: ", error);
+        setFileUploadError(true);
+        setFileUploaded(false);
+    });
+}
+
+function postInspectionRecords(inspectionRecords: InspectionRecord[], objectId: string) {
+    for (const record of inspectionRecords) {
+        if (record.date && record.file) {
+            const formData = new FormData();
+            const dateString = record.date.format("MMM-YY");
+
+            formData.append("file", record.file);
+            formData.append("date", dateString);
+
+            api.post(`/object/${objectId}/inspection-record`, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }).then((response) => {
+                console.log("Inspection record uploaded: ", response.data);
+            }).catch((error) => {
+                console.error("Failed to upload inspection record: ", error);
+            });
+        }
+    }
+}
+
+function postObjectPhoto(objectPhoto: File | null, objectId: string) {
+    if (objectPhoto) {
+        const formData = new FormData();
+
+        formData.append("file", objectPhoto);
+
+        api.post(`/object/${objectId}/photo`, formData, {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        }).then((response) => {
+            console.log("Object photo uploaded: ", response.data);
+        }).catch((error) => {
+            console.error("Failed to upload object photo: ", error);
+        });
+    }
+}
+
 export default function UploadPage() {
 
-    const [file, setFile] = useState<File | null>(null);
+    const [ifcFile, setIfcFile] = useState<File | null>(null);
+    const [objectPhoto, setObjectPhoto] = useState<File | null>(null);
     const [fileName, setFileName] = useState<string>("");
     const [customID, setCustomID] = useState<string>("");
     const [fileUploaded, setFileUploaded] = useState<boolean>(false);
     const [fileUploadError, setFileUploadError] = useState<boolean>(false);
     const [fileUpdatedOrCreated, setFileUpdatedOrCreated] = useState<boolean>(false);
+    const [createdObjectId, setCreatedObjectId] = useState<string>("");
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files[0]) {
-            const newFile = event.target.files[0];
-            setFile(newFile);
-            setFileName(newFile.name);
+    const [inspectionRecords, setInspectionRecords] = useState<{ date: Dayjs | null, file?: File }[]>([{
+        date: null,
+        file: undefined
+    }]);
+
+    const handleIfcFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files?.[0]) {
+            const newIfcFile = event.target.files[0];
+            setIfcFile(newIfcFile);
+            setFileName(newIfcFile.name);
             setFileUploaded(false);
             setFileUploadError(false);
             setFileUpdatedOrCreated(false);
         }
     };
 
-    const handleUpload = () => {
-        if (file) {
-            const formData = new FormData();
-
-            formData.append("file", file);
-            if (customID) formData.append('customID', customID);
-
-            api.post("/object", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data"
-                }
-            }).then((response) => {
-                setFileUploaded(true);
-                setFileUploadError(false);
-
-                if (response.status === 201) setFileUpdatedOrCreated(false); else setFileUpdatedOrCreated(true);
-
-            }).catch((error) => {
-                console.error("Failed to upload file: ", error);
-                setFileUploadError(true);
-                setFileUploaded(false);
-            });
+    const handlePhotoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files?.[0]) {
+            setObjectPhoto(e.target.files[0]);
         }
     }
+
+    const handleUpload = () => {
+        if (ifcFile) {
+            postIFCFile(ifcFile, customID, setFileUploaded, setFileUploadError, setFileUpdatedOrCreated, setCreatedObjectId);
+        }
+    }
+
+    useEffect(() => {
+        if (fileUploaded && createdObjectId) {
+            postInspectionRecords(inspectionRecords, createdObjectId);
+            postObjectPhoto(objectPhoto, createdObjectId);
+        }
+    }, [fileUploaded, createdObjectId, inspectionRecords, objectPhoto]);
 
     const customIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setCustomID(event.target.value);
@@ -169,15 +326,19 @@ export default function UploadPage() {
                 </Box>
 
                 <Box id={"pick-upload-button-box"}>
-                    <Box id={"pick-files-box"} >
-                        <PickFileButton onChange={handleFileChange} StartIcon={IfcIcon} label={"Pick IFC File"}
+                    <Box id={"pick-files-box"}>
+                        <PickFileButton onChange={handleIfcFileChange} StartIcon={<IfcIcon/>} label={"Pick IFC File"}
                                         accept_types={[".ifc"]}/>
 
-                        <PickFileButton onChange={() => (console.log("photo added"))} StartIcon={InsertPhotoIcon} label={"Pick Photo File"}
+                        <PickFileButton onChange={handlePhotoChange} StartIcon={<InsertPhotoIcon/>}
+                                        label={"Pick Photo File"}
                                         accept_types={[".png"]}/>
                     </Box>
 
                     <TextField onChange={customIdChange} label={"Enter Custom ID"}></TextField>
+
+                    <UploadInspectionRecords inspectionRecords={inspectionRecords}
+                                             setInspectionRecords={setInspectionRecords}/>
 
                     <Box>
                         <UploadButton onClick={handleUpload}
@@ -192,4 +353,5 @@ export default function UploadPage() {
             </Box>
         </>
     );
-};
+}
+;
